7fdaf0b3de6f0c40848b561bc2f8a1af2eed6628
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-11-13 12:11:49 +0200

    feat: Style responsive image gallery with grid and hover effects

--- js/meme-service.js
+++ js/meme-service.js
@@ -4,13 +4,29 @@ var gImgs = [
         id: 1,
         url: 'img/1.jpg',
         keywords: ['funny', 'cat']
-
     },
     {
         id: 2,
         url: 'img/2.jpg',
         keywords: ['funny', 'dog']
-    }]
+    },
+    { id: 3, url: 'img/3.jpg', keywords: ['meme'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['meme'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['meme'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['meme'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['meme'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['meme'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['meme'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['meme'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['meme'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['meme'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['meme'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['meme'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['meme'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['meme'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['meme'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['meme'] }
+]
 
 var gMeme = {
     selectedImgId: 1,
